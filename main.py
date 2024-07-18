8c3e0268bb682230e85f2a971b818af1d947d9f5
Kashish Jain <37466111+kashishj197@users.noreply.github.com>
2024-07-18 09:48:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+import random
+
+choices = [rock, paper, scissors]
+player = input("What do you choose? Type 0 for rock 1 for paper and 2 for scissors: ")
+computer = random.randint(0, 2)
+print(f"You chose\n{choices[player]}")
+print(f"Computer chose\n{choices[computer]}")
+
+if player == 0 and computer == 2:
+  print("You win!")
+elif computer > player:
+  print("You lose!")
+else
+  print("It's a draw!")
+
+
+
+
+
 
