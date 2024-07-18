d5d28c271ffc7f2117573ff81133b8be5758c32b
Kashish Jain <37466111+kashishj197@users.noreply.github.com>
2024-07-18 10:01:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,13 +28,13 @@ import random
 
 choices = [rock, paper, scissors]
 player = int(input("What do you choose? Type 0 for rock 1 for paper and 2 for scissors: "))
+if player > 2 or player < 0:
+  print("You typed an invalid number!")
 computer = random.randint(0, 2)
 print(f"You chose\n{choices[player]}")
 print(f"Computer chose\n{choices[computer]}")
 
-if player > 2 or player < 0:
-  print("You typed an invalid number!")
-elif player == 0 and computer == 2:
+if player == 0 and computer == 2:
   print("You win!")
 elif computer == 0 and player == 1:
   print("You win!")
